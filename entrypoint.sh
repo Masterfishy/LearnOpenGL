9d847a76fd6969bb1edfa06ec3154d30d86a1b20
Zach Long <iamzachlong@gmail.com>
2025-05-08 16:52:43 -0400

    Started building hosting container

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start nginx
+nginx
+
+# Watches for changes to the build artifacts
+echo "Watching for changes..."
+while true; do
+    inotifywait -r -e modify,create,delete,move /usr/share/nginx/html
+    echo "Change detected, reloading nginx..."
+    nginx -s reload
+done
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 
+// Emscripten includes
+#include <emscripten.h>
 #include <GLFW/glfw3.h>
+
+// System includes
+#include <iostream>
+
+GLFWwindow* gWindow;
+
+void mainLoop()
+{
+    if (gWindow == NULL)
+    {
+        return;
+    }
+
+    while (!glfwWindowShouldClose(gWindow))
+    {
+        glfwSwapBuffers(gWindow);
+        glfwPollEvents();
+    }
+}
 
 int main()
 {
@@ -8,5 +29,20 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    gWindow = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (gWindow == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(gWindow);
+
+    glViewport(0, 0, 800, 600);
+
+    emscripten_set_main_loop(mainLoop, 0, false);
+
+    glfwTerminate();
     return 0;
 }
